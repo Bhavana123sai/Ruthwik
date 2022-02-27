96647ec0312704b3873a01c26ff116115f54b74d
Bhavana sai <chikita2sai@gmail.com>
2022-02-27 16:53:31 +0530

    complete the code

---
+++ MainFile.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class MainFile {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a=30,b=5;
+    }
+}
+abstract class Cal{
+    abstract  public int add(int a,int b);
+    abstract  public int sub(int a,int b);
+    abstract public int mul(int a, int b);
+}
