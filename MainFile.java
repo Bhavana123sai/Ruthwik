ea4e67024f9885cba4a546105bd2f91afa9b2083
Bhavana sai <chikita2sai@gmail.com>
2022-02-27 17:31:50 +0530

    completed the add function code

--- MainFile.java
+++ MainFile.java
@@ -1,14 +1,32 @@
+abstract class Sum{
+    /* These two are abstract methods, the child class
+     * must implement these methods
+     */
+    public abstract int add(int n1, int n2);
+    public abstract int sub(int n1, int n2);
+    public abstract int mul(int n1, int n2);
 
-import java.util.Scanner;
-public class MainFile {
-
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int a=30,b=5;
+    //Regular method
+    public void disp(){
+        System.out.println("Method of class Sum");
     }
 }
-abstract class Cal{
-    abstract  public int add(int a,int b);
-    abstract  public int sub(int a,int b);
-    abstract public int mul(int a, int b);
+//Regular class extends abstract class
+class MainFile extends Sum{
+
+    /* If I don't provide the implementation of these two methods, the
+     * program will throw compilation error.
+     */
+    public int add(int a, int b){
+         return a+b;
+    }
+
+
+    public static void main(String args[]){
+        Sum obj = new MainFile();
+        System.out.println(obj.add(3, 7));
+        System.out.println(obj.sub(4, 3));
+        System.out.println(obj.mul(4, 4));
+        obj.disp();
+    }
 }
